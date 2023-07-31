6eb684a57be4f20e159c8195a6f97ff9adca0418
Andriy <andygrigs88@gmail.com>
2023-07-31 10:16:38 +0300

    adding babel

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,28 +6,18 @@ import { AppRouter } from './router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
 
-import { useTranslation } from 'react-i18next';
+
 
 
 const App = () => {
-  const { t, i18n } = useTranslation();
 
-  const handleLanguageChange = (language: string) => {
-    i18n.changeLanguage(language);
-  };
+
   const [theme] = useTheme()
   return (
     <div className={classNames('app', {}, [theme])}>
       <Navbar />
       <div className='content-page'>
-        <div>
-          <h1>{t('welcome')}</h1>
-          <p>{t('greeting', { name: 'John' })}</p>
-          <div>
-            <button onClick={() => handleLanguageChange('en')}>English</button>
-            <button onClick={() => handleLanguageChange('es')}>Spanish</button>
-          </div>
-        </div>
+
         <Sidebar />
         <AppRouter />
       </div>
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+
 
 const MainPage = () => {
+  const { t, i18n } = useTranslation();
+
+  const handleLanguageChange = (language: string) => {
+    i18n.changeLanguage(language);
+  };
+
   return (
-    <div>MainPage</div>
+    <div>
+      <div> MainPage </div>
+      <div>
+        <h1>{t('welcome')}</h1>
+        <p>{t('greeting', { name: 'John' })}</p>
+        <div>
+          <button onClick={() => handleLanguageChange('en')}>English</button>
+          <button onClick={() => handleLanguageChange('es')}>Spanish</button>
+        </div>
+      </div>
+    </div>
   );
 }
 
