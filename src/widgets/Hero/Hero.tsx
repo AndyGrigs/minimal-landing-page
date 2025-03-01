8c4b9773d991f9494834b06279924684299f0679
Andriy <andygrigs88@gmail.com>
2025-03-01 01:00:45 +0100

    adding image

--- src/widgets/Hero/Hero.tsx
+++ src/widgets/Hero/Hero.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import cls from "./Hero.module.scss"
 import {classNames} from "sheared/lib/className"
+import image from "../../sheared/assets/images/hero-mobile.png"
 
 export interface HeroProps {
 className?: string;
@@ -8,7 +9,9 @@ className?: string;
 
 export const Hero = ({className}:HeroProps)=>  {
   return (
-    <div className={classNames(cls.Hero, {}, [className, 'component'])}>
+    <div className={classNames(cls.Hero, {}, [className, 'component'])} 
+        style={{backgroundImage: `url(${image})`}}
+    >
       <div>
         <p>Modern and minimalist furniture for you</p>
         <p>Add a touch of elegance to your space with handcrafted furniture pieces</p>
